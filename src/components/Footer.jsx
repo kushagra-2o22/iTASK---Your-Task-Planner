c8ca8d1f06a8ce617cbb89c079d4eb8f8c9a6928
KUSHAGRA SINGH <117945509+kushagra-2o22@users.noreply.github.com>
2024-07-13 14:53:04 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className="flex justify-center font-bold">
+        <h5>Made with
+ <span className="text-red-600">♥ </span>
+BY
+  KUSHAGRA SINGH</h5>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
